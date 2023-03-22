ac310b0938afd8f0c1b1621e50e554311451dceb
tqk2811 <tqk2811@gmail.com>
2023-03-22 11:18:25 +0700

    update unit test

--- TqkLibrary.GSM.Test/TestGsmCommandResponse.cs
+++ TqkLibrary.GSM.Test/TestGsmCommandResponse.cs
@@ -9,7 +9,7 @@ namespace TqkLibrary.GSM.Test
     public class TestGsmCommandResponse
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestCommandResponse()
         {
             var test_str = "+CMGR: \"arg1\",arg2,\"\",\"2022/06/10 17:42:17+28\",\"arg ,with\nlinebreak\"\r\nthis is \r\ndataa";
             GsmCommandResponse gsmCommandResponse = GsmCommandResponse.Parse(test_str);
@@ -21,7 +21,7 @@ namespace TqkLibrary.GSM.Test
         }
 
         [TestMethod]
-        public void TestMethod2()
+        public void Test_TestResponse()
         {
             var test_str = "+CMGR: (\"ab c\",\"de f\", ghi),(0-5)";
             GsmCommandResponse gsmCommandResponse = GsmCommandResponse.Parse(test_str);
@@ -30,8 +30,9 @@ namespace TqkLibrary.GSM.Test
             Assert.IsTrue(gsmCommandResponse.Options.Count() == 2);
             Assert.IsTrue(gsmCommandResponse.Options.Any(x => x.Any(y => y.Equals("\"ab c\""))));
         }
+
         [TestMethod]
-        public void TestMethod3()
+        public void TestParseConnectBinary()
         {
             //var test_str = "\r\nCONNECT\r\n\xab\xff\x34\xac\r\n+QFDWL: 20,3\r\n\r\n+QFDWL: 20,3\r\n"; //will bug here, but supper rate
             var test_str = "\r\nCONNECT\r\n\xab\xff\x34\xac\r\n+QFDWL: 20,3\r\n\xff\r\n+QFDWL: 20,3\r\n";
@@ -46,20 +47,20 @@ namespace TqkLibrary.GSM.Test
 
 
         [TestMethod]
-        public void TestMethod4()
+        public void TestParseAT()
         {
             //Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
             Encoding ISO_8859_1 = Encoding.GetEncoding("ISO-8859-1");
 
             byte[] qfdwl_file = File.ReadAllBytes("QFDWL.file");
-            string str_1252 = ISO_8859_1.GetString(qfdwl_file);
+            string str_ISO_8859_1 = ISO_8859_1.GetString(qfdwl_file);
             string str_ascii = Encoding.ASCII.GetString(qfdwl_file);
-            Assert.AreEqual(str_1252.Length, str_ascii.Length);
-            Assert.IsTrue(qfdwl_file.SequenceEqual(ISO_8859_1.GetBytes(str_1252)));
+            Assert.AreEqual(str_ISO_8859_1.Length, str_ascii.Length);
+            Assert.IsTrue(qfdwl_file.SequenceEqual(ISO_8859_1.GetBytes(str_ISO_8859_1)));
 
             string[] test_arr = new string[] {
                 str_ascii,
-                str_1252,
+                str_ISO_8859_1,
                 "AT+QFDWL=\"RAM:sound.wav\"\r\r\nCONNECT\r\n\xab\xff\0\0\0\x34\xac\r\n+QFDWL: 20,3\r\n\r\nOK\r\n",
                 "AT+COPS?\r\r\n+COPS: 0,0,\"VINAPHONE\"\r\n\r\nOK\r\n",
                 "AT+QFDWL=\"RAM:voicea.wav\"\r\r\n+CME ERROR: 4010\r\n",
@@ -73,14 +74,24 @@ namespace TqkLibrary.GSM.Test
         }
 
         [TestMethod]
-        public void TestMethod5()
+        public void TestParseDownloadedFile()
         {
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            byte[] buffer = new byte[] { 0xab, 0xff, 0x34, 0xac };
-            string str = Encoding.GetEncoding(1252).GetString(buffer);
-            Assert.IsTrue(str.Select(x => (byte)x).SequenceEqual(buffer));
-            Assert.IsTrue(Encoding.GetEncoding(1252).GetBytes(str).SequenceEqual(buffer));
+            Encoding ISO_8859_1 = Encoding.GetEncoding("ISO-8859-1");
+            byte[] qfdwl_file = File.ReadAllBytes("QFDWL.file");
+            string str_ISO_8859_1 = ISO_8859_1.GetString(qfdwl_file);
+            Regex regex = new Regex("^(AT.*?\r)(\r\n[\\x00-\\xFF]*?\r\n|)\r\n(OK|ERROR|\\+CM. ERROR:.*?)\r\n$", RegexOptions.None);
+            Match match = regex.Match(str_ISO_8859_1);
+            Assert.IsTrue(match.Success);
+
+            GsmCommandResponse gsmCommandResponse = GsmCommandResponse.Parse(match.Groups[2].Value);
+            Assert.IsNotNull(gsmCommandResponse);
+            Assert.AreEqual(gsmCommandResponse.Arguments.Count(), 2);
+            var binarySize = int.Parse(gsmCommandResponse.Arguments.First());
+            Assert.AreEqual(binarySize, gsmCommandResponse.BinaryData.Count());
+            byte[] checksum = gsmCommandResponse.Arguments.Last().HexStringToByteArray();
+            Assert.AreEqual(checksum.Length, 2);
+            byte[] calcCheckSum = gsmCommandResponse.BinaryData.ToArray().CheckSum();
+            Assert.IsTrue(calcCheckSum.SequenceEqual(checksum));
         }
     }
 }
