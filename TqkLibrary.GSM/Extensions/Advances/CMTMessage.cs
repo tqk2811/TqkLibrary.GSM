424ebe9e0dee795f6e055aa1dac304fa23dde2cf
tqk2811 <tqk2811@gmail.com>
2023-04-15 21:03:54 +0700

    add xml docs

--- TqkLibrary.GSM/Extensions/Advances/CMTMessage.cs
+++ TqkLibrary.GSM/Extensions/Advances/CMTMessage.cs
@@ -8,8 +8,17 @@ using System.Threading.Tasks;
 
 namespace TqkLibrary.GSM.Extensions.Advances
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public static class CMTMessageExtension
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="gsmClient"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         public static async Task<CMTMessage> RegisterMessageAsync(this GsmClient gsmClient, CancellationToken cancellationToken = default)
         {
             if (await gsmClient.CNMI().WriteAsync(CNMI_Mode.Class2, CNMI_MT.SmsDeliver, cancellationToken).ConfigureAwait(false) &&
@@ -19,27 +28,46 @@ namespace TqkLibrary.GSM.Extensions.Advances
             }
             return null;
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="gsmClient"></param>
+        /// <returns></returns>
         public static CMTMessage RegisterMessage(this GsmClient gsmClient)
             => new CMTMessage(gsmClient);
     }
 
-
+    /// <summary>
+    /// 
+    /// </summary>
     public class CMTMessage : IDisposable
     {
         readonly GsmClient gsmClient;
+        /// <summary>
+        /// 
+        /// </summary>
         public event Action<ISms> OnSmsReceived;
         readonly Dictionary<byte, List<Message>> pdu_cache = new Dictionary<byte, List<Message>>();
         readonly AsyncLock asyncLock = new AsyncLock();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="gsmClient"></param>
         public CMTMessage(GsmClient gsmClient)
         {
             this.gsmClient = gsmClient;
             gsmClient.OnCommandResponse += GsmClient_OnCommandResponse;
         }
+        /// <summary>
+        /// 
+        /// </summary>
         ~CMTMessage()
         {
             Dispose(false);
         }
+        /// <summary>
+        /// 
+        /// </summary>
         public void Dispose()
         {
             Dispose(true);
@@ -61,101 +89,114 @@ namespace TqkLibrary.GSM.Extensions.Advances
         {
             try
             {
-            switch (await gsmClient.CMGF().ReadAsync())
-            {
-                case MessageFormat.PduMode:
-                    //[<alpha>],<length><CR><LF><pdu>
-                    PDU pdu = PDU.TryParse(commandData.Data.HexStringToByteArray());
-                    if (pdu?.PduHeader?.Type == PduType.SmsDeliver)
-                    {
-                        Message message = new Message(pdu);
-                        if (message.IsSplit)
+                switch (await gsmClient.CMGF().ReadAsync())
+                {
+                    case MessageFormat.PduMode:
+                        //[<alpha>],<length><CR><LF><pdu>
+                        PDU pdu = PDU.TryParse(commandData.Data.HexStringToByteArray());
+                        if (pdu?.PduHeader?.Type == PduType.SmsDeliver)
                         {
-                            using (await asyncLock.LockAsync().ConfigureAwait(false))
+                            Message message = new Message(pdu);
+                            if (message.IsSplit)
                             {
-                                if (pdu_cache.ContainsKey(pdu.UDH.CSMSReferenceNumber))
+                                using (await asyncLock.LockAsync().ConfigureAwait(false))
                                 {
-                                    pdu_cache[pdu.UDH.CSMSReferenceNumber].Add(message);
-                                    if (pdu_cache[pdu.UDH.CSMSReferenceNumber].Count == pdu.UDH.TotalNumberOfParts)
+                                    if (pdu_cache.ContainsKey(pdu.UDH.CSMSReferenceNumber))
                                     {
-                                        SmsPdu smsPdu = new SmsPdu(pdu_cache[pdu.UDH.CSMSReferenceNumber]);
-                                        pdu_cache.Remove(pdu.UDH.CSMSReferenceNumber);
-                                        OnSmsReceived?.Invoke(smsPdu);
+                                        pdu_cache[pdu.UDH.CSMSReferenceNumber].Add(message);
+                                        if (pdu_cache[pdu.UDH.CSMSReferenceNumber].Count == pdu.UDH.TotalNumberOfParts)
+                                        {
+                                            SmsPdu smsPdu = new SmsPdu(pdu_cache[pdu.UDH.CSMSReferenceNumber]);
+                                            pdu_cache.Remove(pdu.UDH.CSMSReferenceNumber);
+                                            OnSmsReceived?.Invoke(smsPdu);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        pdu_cache[pdu.UDH.CSMSReferenceNumber] = new List<Message>() { message };
                                     }
                                 }
-                                else
-                                {
-                                    pdu_cache[pdu.UDH.CSMSReferenceNumber] = new List<Message>() { message };
-                                }
+                            }
+                            else
+                            {
+                                SmsPdu smsPdu = new SmsPdu(message);
+                                OnSmsReceived?.Invoke(smsPdu);
                             }
                         }
-                        else
+                        break;
+
+                    case MessageFormat.TextMode:
+                        // +CMT:<oa>,<alpha>,<scts>[,<tooa>,<fo>,<pid>,<dcs>,<sca>,<tosca>,<length>]<CR><LF><data>
+                        if ((commandData.Arguments.Count() == 3 || commandData.Arguments.Count() == 10) && !string.IsNullOrWhiteSpace(commandData.Data))
                         {
-                            SmsPdu smsPdu = new SmsPdu(message);
-                            OnSmsReceived?.Invoke(smsPdu);
+                            Sms sms = new Sms();
+                            sms.From = commandData.Arguments.FirstOrDefault()?.Trim('"');
+                            //sms.AlphanumericRepresentation = commandData.Arguments.Skip(1).FirstOrDefault()?.Trim('"');
+                            if (DateTime.TryParse(commandData.Arguments.Skip(2).FirstOrDefault(), out DateTime r)) sms.ArrivalTime = r;
+                            sms.Message = commandData.Data;
+                            OnSmsReceived?.Invoke(sms);
                         }
-                    }
-                    break;
+                        break;
 
-                case MessageFormat.TextMode:
-                    // +CMT:<oa>,<alpha>,<scts>[,<tooa>,<fo>,<pid>,<dcs>,<sca>,<tosca>,<length>]<CR><LF><data>
-                    if ((commandData.Arguments.Count() == 3 || commandData.Arguments.Count() == 10) && !string.IsNullOrWhiteSpace(commandData.Data))
-                    {
-                        Sms sms = new Sms();
-                        sms.From = commandData.Arguments.FirstOrDefault()?.Trim('"');
-                        //sms.AlphanumericRepresentation = commandData.Arguments.Skip(1).FirstOrDefault()?.Trim('"');
-                        if (DateTime.TryParse(commandData.Arguments.Skip(2).FirstOrDefault(), out DateTime r)) sms.ArrivalTime = r;
-                        sms.Message = commandData.Data;
-                        OnSmsReceived?.Invoke(sms);
-                    }
-                    break;
-
-                default:
-                    break;
-            }
+                    default:
+                        break;
+                }
             }
             catch (Exception ex)
             {
 #if DEBUG
                 Console.WriteLine($"{ex.GetType().FullName}: {ex.Message}, {ex.StackTrace}");
 #endif
+            }
         }
-    }
-    public interface ISms
-    {
-        string From { get; }
-        string Message { get; }
-        DateTime? ArrivalTime { get; }
-    }
-
-    internal class Sms : ISms
-    {
-        public string From { get; set; }
-
-        public string Message { get; set; }
-
-        public DateTime? ArrivalTime { get; set; }
-    }
-
-    internal class SmsPdu : ISms
-    {
-        readonly List<Message> messages = new List<Message>();
-        public SmsPdu(Message message)
+        /// <summary>
+        /// 
+        /// </summary>
+        public interface ISms
         {
-            messages.Add(message ?? throw new ArgumentNullException(nameof(message)));
+            /// <summary>
+            /// 
+            /// </summary>
+            string From { get; }
+            /// <summary>
+            /// 
+            /// </summary>
+            string Message { get; }
+            /// <summary>
+            /// 
+            /// </summary>
+            DateTime? ArrivalTime { get; }
         }
-        public SmsPdu(IEnumerable<Message> messages)
+
+        internal class Sms : ISms
         {
-            if (messages.Any(x => x == null || !x.IsSplit || x.SplitCount != messages.Count()) ||
-                messages.GroupBy(x => x.SplitId).Count() != 1 ||
-                messages.GroupBy(x => x.SplitIndex).Count() != messages.Count())
-                throw new InvalidOperationException($"messages is wrong");
+            public string From { get; set; }
 
-            this.messages.AddRange(messages.OrderBy(x => x.SplitIndex));
+            public string Message { get; set; }
+
+            public DateTime? ArrivalTime { get; set; }
         }
 
-        public string From => messages.First().SenderNumber;
-        public string Message => string.Join("", messages.Select(x => x.Content));
-        public DateTime? ArrivalTime => messages.First().DateTime;
+        internal class SmsPdu : ISms
+        {
+            readonly List<Message> messages = new List<Message>();
+            public SmsPdu(Message message)
+            {
+                messages.Add(message ?? throw new ArgumentNullException(nameof(message)));
+            }
+            public SmsPdu(IEnumerable<Message> messages)
+            {
+                if (messages.Any(x => x == null || !x.IsSplit || x.SplitCount != messages.Count()) ||
+                    messages.GroupBy(x => x.SplitId).Count() != 1 ||
+                    messages.GroupBy(x => x.SplitIndex).Count() != messages.Count())
+                    throw new InvalidOperationException($"messages is wrong");
+
+                this.messages.AddRange(messages.OrderBy(x => x.SplitIndex));
+            }
+
+            public string From => messages.First().SenderNumber;
+            public string Message => string.Join("", messages.Select(x => x.Content));
+            public DateTime? ArrivalTime => messages.First().DateTime;
+        }
     }
 }
