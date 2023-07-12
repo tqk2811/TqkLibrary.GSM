bcf6267e7008e39d1b207743f6c05680bd6599a8
tqk2811 <tqk2811@gmail.com>
2023-07-12 17:38:31 +0700

    add PromptEvent

---
+++ TqkLibrary.GSM/PromptEvent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace TqkLibrary.GSM
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class PromptEvent
+    {
+        readonly Stream _stream;
+        internal PromptEvent(Stream stream)
+        {
+            _stream = stream ?? throw new ArgumentNullException(nameof(stream));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool Handled { get; set; } = false;
+
+        /// <summary>
+        /// To send the message issue Ctrl-Z char (0x1A hex).
+        /// </summary>
+        public void SendCtrlZ()
+        {
+            _stream.WriteByte(0x1a);
+        }
+
+        /// <summary>
+        /// To exit without sending the message issue ESC char (0x1B hex).
+        /// </summary>
+        public void SendEsc()
+        {
+            _stream.WriteByte(0x1b);
+        }
+    }
+}
