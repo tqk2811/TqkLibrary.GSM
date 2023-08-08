9245baa861f63b96c4b6e82d7951367a8e9cf3a7
tqk2811 <tqk2811@gmail.com>
2023-08-08 11:57:43 +0700

    add cusd with line break test

---
+++ TqkLibrary.GSM.Test/AtClientLoopReadLine_Parse_Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TqkLibrary.GSM.AtClient;
+
+namespace TqkLibrary.GSM.Test
+{
+    [TestClass]
+    public class AtClientLoopReadLine_Parse_Test
+    {
+        [TestMethod]
+        public async Task TestAsync()
+        {
+            AtClientLoopReadLine atClientLoopReadLine = new AtClientLoopReadLine("abc");
+            await atClientLoopReadLine._ParseLine("+CUSD: 2,\"Ban dang o MSHN02H. So may cua ban la:\r84936514135\",15");
+        }
+    }
+}
