2873638de658a2200d88fd445e1459ec19ce8d9e
tqk2811 <tqk2811@gmail.com>
2023-09-06 17:11:52 +0700

    add ValidityPeriodFormat

---
+++ TqkLibrary.GSM/Helpers/PduPaser/ValidityPeriodFormat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TqkLibrary.GSM.Helpers.PduPaser
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum ValidityPeriodFormat : byte
+    {
+        /// <summary>
+        /// not present
+        /// </summary>
+        NotPresent = 0x00,
+    }
+}
