008d47845db4b94632ea6c979fd9aedb3024e53c
tqk2811 <tqk2811@gmail.com>
2023-09-08 16:14:10 +0700

    update ValidityPeriodFormat

--- TqkLibrary.GSM/Helpers/PduPaser/ValidityPeriodFormat.cs
+++ TqkLibrary.GSM/Helpers/PduPaser/ValidityPeriodFormat.cs
@@ -7,13 +7,26 @@ using System.Threading.Tasks;
 namespace TqkLibrary.GSM.Helpers.PduPaser
 {
     /// <summary>
-    /// 
+    /// 9.2.3.3 TP-Validity-Period-Format (TP-VPF)<br>
+    /// </br>The TP-Validity-Period-Format is a 2-bit field, located within bit no 3 and 4 of the first octet of <see cref="PduType.SmsSubmit"/>
     /// </summary>
     public enum ValidityPeriodFormat : byte
     {
         /// <summary>
-        /// not present
+        /// TP-VP field not present
         /// </summary>
-        NotPresent = 0x00,
+        NotPresent = 0b00000000,
+        /// <summary>
+        /// Reserved
+        /// </summary>
+        Reserved = 0b00000001,
+        /// <summary>
+        /// TP-VP field present and integer represented (relative)
+        /// </summary>
+        PresentAndIntegerRepresented = 0b00000010,
+        /// <summary>
+        /// TP-VP field present and semi-octet represented (absolute)
+        /// </summary>
+        PresentAndSemiOctetRepresented = 0b00000011,
     }
 }
