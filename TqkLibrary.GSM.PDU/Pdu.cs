56b3a22e75e3e661a13738ffe1b80820c5d6487f
tqk2811 <tqk2811@gmail.com>
2023-10-07 18:01:22 +0700

    Pdu fix npi, ton

--- TqkLibrary.GSM.PDU/Pdu.cs
+++ TqkLibrary.GSM.PDU/Pdu.cs
@@ -219,6 +219,16 @@ namespace TqkLibrary.GSM.PDU
             }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="desNumber">
+        /// +xxxx ( International )<br></br>
+        /// 0xxx ( National )
+        /// </param>
+        /// <param name="message"></param>
+        /// <param name="ValidityPeriod"></param>
+        /// <returns></returns>
         public static IEnumerable<Pdu> Create(
             string desNumber, string message, TimeSpan? ValidityPeriod = null
             )
@@ -227,6 +237,9 @@ namespace TqkLibrary.GSM.PDU
                 ValidityPeriod = TimeSpan.Zero;
 
             bool isNumber = desNumber.All(x => x == '+' || (x >= '0' && x <= '9'));
+            if (!isNumber)
+                throw new InvalidDataException($"'{desNumber}' requied number and/or '+' only");
+
             bool isUnicode = message.Any(x => x > 128);
 
             IEncodeDecode encodeDecode = isUnicode ? UnicodeEncrypt.Instance : SevenBitEncrypt.Instance;
@@ -261,9 +274,15 @@ namespace TqkLibrary.GSM.PDU
                     AddressLength = (byte)desNumber.TrimStart('+').Length,
                     //If a subscriber enters a telephone number with `+' sign at its start, the `+' sign will be removed and the address gets TON=1 (international number), NPI=1.
                     //The number itself must always start with a country code and must be formatted exactly according to the E.164 standard.
-                    NPI = isNumber ? NumberingPlanIdentification.ISDNTelephoneNumberingPlan : NumberingPlanIdentification.Unknown,
-                    TON = isNumber ? TypeOfNumber.Unknown : TypeOfNumber.Alphanumeric,
-                    Address = isNumber ? desNumber.Trim('+').ToDecimalSemiOctets() : SevenBitEncrypt.Instance.Encode(desNumber),
+                    //
+                    //In contrast, for numbers written without `+' sign the address gets TON=0 (unknown), NPI=1.
+                    //In this case the number must adhere to the mobile operator's dial plan,
+                    //which means that international numbers must have the international prefix
+                    //(00 in most countries, but 011 in the USA) before the country code and numbers for long-distance calls must start with the trunk prefix
+                    //(0 in most countries, 1 in the USA) followed by a trunk code.
+                    NPI = NumberingPlanIdentification.ISDNTelephoneNumberingPlan,
+                    TON = desNumber.StartsWith("+") ? TypeOfNumber.InternationalNumber : TypeOfNumber.Unknown,
+                    Address = desNumber.Trim('+').ToDecimalSemiOctets(),
                 };
                 pdu.ProtocalIdentifier = 0;
                 pdu.DataCodingScheme = new DataCodingScheme(0x00);
@@ -301,9 +320,9 @@ namespace TqkLibrary.GSM.PDU
                 //0011000A01 3018111111 00 00 32901112008282 17 C8329BFD064D9B5362999DB697E565B96BFC6E8700
                 //test 2
                 //                          time -  data length - udh - dataaaaa
-                //0051000A01 3018111111 00 08 32901112111482 46 050003970300 005200FA00740020006C007500690020002C0020006B006800F4006E006700200072006100200074006800EA006D002000731EA3006E0020007000681EA9006D0020006E00EA006E0020006300F30020007400681EDD00690020006700690061006E0020006300680061007500200063006800751ED10074002000630068006F002000621EA3006E00200074
-                //0051000A01 3018111111 00 08 32901112316082 46 050003100301 006800E2006E002C0020006E00EA006E0020006E006800EC006E0020006E006701B01EDD00690020006300F30020007400ED0020006301A1002000721ED300690020002C00200074006100790020006E00E000790020006300681EAF00630020006D1ED90074002000761EE300740020006C01690020007200751ED30069002000621EC700700020006400ED
-                //0051000A01 3018111111 00 08 32901112316082 1B 050003100302 0020002C0020006300F2006E0020007200751ED3006900200063006800610020006300681EAF00630020006D1ED900740020007600E3
+                //0051000A01 3018111111 00 08 17 46 050003970300 005200FA00740020006C007500690020002C0020006B006800F4006E006700200072006100200074006800EA006D002000731EA3006E0020007000681EA9006D0020006E00EA006E0020006300F30020007400681EDD00690020006700690061006E0020006300680061007500200063006800751ED10074002000630068006F002000621EA3006E00200074
+                //0051000A01 3018111111 00 08 17 46 050003100301 006800E2006E002C0020006E00EA006E0020006E006800EC006E0020006E006701B01EDD00690020006300F30020007400ED0020006301A1002000721ED300690020002C00200074006100790020006E00E000790020006300681EAF00630020006D1ED90074002000761EE300740020006C01690020007200751ED30069002000621EC700700020006400ED
+                //0051000A01 3018111111 00 08 17 1B 050003100302 0020002C0020006300F2006E0020007200751ED3006900200063006800610020006300681EAF00630020006D1ED900740020007600E3
                 string hex = BitConverter.ToString(pdu.GetBytes().ToArray()).Replace("-", string.Empty);
 #endif
                 yield return pdu;
